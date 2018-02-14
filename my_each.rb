0a910a0ac54778f2a8bfca0c6f555e0a29c0d5cc
Eddie Moon <eddiemoonie@gmail.com>
2018-02-14 19:58:15 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -9,7 +9,16 @@ def my_each(array)
   array
 end
 
-array = [1, 2, 3, 4]
-my_each(array) do |i|
-  puts i
-end
+#array = [1, 2, 3, 4]
+#my_each(array) do |i|
+#  puts i
+#end
+
+# OUTPUT
+# 1
+# 2
+# 3
+# 4
+
+# RETURN
+# [1, 2, 3, 4]
